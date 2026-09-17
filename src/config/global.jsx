dd98bc003a281915131852466dba73e9d0b150cd
Muhammad Moazzam <moazzambhatti186@gmail.com>
2026-09-17 15:26:43 -0700

    Authentication Completed with Backend

--- src/config/global.jsx
+++ src/config/global.jsx
@@ -1,3 +1,5 @@
 import { message } from "antd";
 
 window.toastify = (msg,type) => message[type](msg)
+
+window.getRandomId = ()=> Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
